a8fdbc066f9aa3e54709750cdb435cc08ad1de9e
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-03 20:26:57 -0600

    x

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,11 @@
 class Discount < ApplicationRecord
   validates_presence_of :discount, :amount
   belongs_to :merchant
+
+  def self.selected_discounts(item_quantity)
+    
+    binding.pry
+    
+  end
+  
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -9,5 +9,26 @@ RSpec.describe Discount do
         it { should validate_presence_of(:discount) }
         it { should validate_presence_of(:amount) }
     end
+    describe "methods" do
+        before :each do
+            @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+            @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+            @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+            @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 12 )
+            @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 30 )
+    
+            @discount_1 = @megan.discounts.create(discount: 2, amount: 5)
+            @discount_2 = @megan.discounts.create(discount: 5, amount: 10)
+            @discount_3 = @megan.discounts.create(discount: 10, amount: 20)
+    
+            @bulk = DiscountCalculator.new(@megan.discounts)
+    
+          end
+        it '#discounts_selected' do
+            expect(@megan.discounts.selected_discounts(6)).to eq(1)   
+            
+        end
+        
+    end
     
 end
