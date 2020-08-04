446799a49c4484ebe0e1d7136fd56d159bd5e029
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-04 13:00:22 -0600

    A the destroy action.

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -27,6 +27,14 @@ class Merchant::DiscountsController  < Merchant::BaseController
         flash[:error] = "Check inputs. The value should be bumbers greater than 0and less than 100 on discount."
         redirect_to edit_merchant_discount_path(discount)
     end
+
+    def destroy
+        discount = Discount.find(params[:id])
+        discount.destroy
+        flash[:success] = "Discount deleted"
+        redirect_to merchant_dashboard_path
+    end
+    
     
     private
     def discount_params
