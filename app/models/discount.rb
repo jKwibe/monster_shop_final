f585acb7f755eb7fc174a2dfd595683332253392
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-03 22:09:35 -0600

    Add the discount query.

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,8 @@
 class Discount < ApplicationRecord
   validates_presence_of :discount, :amount
   belongs_to :merchant
-  
+
+  def self.best_discounts(quantity)
+    where('amount <= ?', quantity).order(discount: :desc).pluck(:discount).first
+  end
 end
