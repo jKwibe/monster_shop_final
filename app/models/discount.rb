4718965907f8c7673afd20ef36041455f8f29405
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-03 20:55:32 -0600

    remove junk

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,11 +1,5 @@
 class Discount < ApplicationRecord
   validates_presence_of :discount, :amount
   belongs_to :merchant
-
-  def self.selected_discounts(item_quantity)
-    
-    binding.pry
-    
-  end
   
 end
