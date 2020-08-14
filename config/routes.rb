80d3b4ce925b4ead1c4a92ab44115fbdf768f518
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-08-14 14:04:15 -0600

    Change Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,31 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :root, to: 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  # resources :merchants do
+  #   resources :items, only: [:index]
+  # end
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  get '/merchants/new', to: 'merchants#new'
+  get '/merchants/:id', to: 'merchants#show'
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  delete '/merchants/:id', to: 'merchants#destroy'
+  patch '/merchants/:id', to: 'merchants#update'
+  post '/merchants', to: 'merchants#create'
 
-  resources :reviews, only: [:edit, :update, :destroy]
+  # resources :items, only: [:index, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show'
+  get '/items/reviews/new', to: 'reviews#new'
+  post '/items/reviews', to: 'reviews#create'
+
+  # resources :reviews, only: [:edit, :update, :destroy]
+  get '/reviews/:id/edit', to: 'reviews#edit'
+  patch '/reviews/:id', to: 'reviews#update'
+  delete '/reviews/:id', to: 'reviews#destroy'
+
 
   get '/cart', to: 'cart#show'
   post '/cart/:item_id', to: 'cart#add_item'
@@ -19,7 +35,11 @@ Rails.application.routes.draw do
   delete '/cart/:item_id', to: 'cart#remove_item'
 
   get '/registration', to: 'users#new', as: :registration
-  resources :users, only: [:create, :update]
+  # resources :users, only: [:create, :update]
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
   patch '/user/:id', to: 'users#update'
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
@@ -35,17 +55,40 @@ Rails.application.routes.draw do
 
   namespace :merchant do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :orders, only: :show
-    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    # resources :orders, only: :show
+    get '/orders/:id', to: 'orders#show'
+
+    # resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/items/new', to: 'items#new'
+    get '/items', to: 'items#index'
+    post '/items', to: 'items#create'
+    get '/items/:id/edit', to: 'items#edit'
+    put '/items/:id', to: 'items#update'
+    patch '/items/:id', to: 'items#update'
+    delete '/items/:id', to: 'items#destroy'
+
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
-    resources :discounts, except: [:index]
+    # resources :discounts, except: [:index]
+     get '/discounts/new', to: 'discounts#new'
+     get '/discounts/:id', to: 'discounts#show'
+     get '/discounts/:id/edit', to: 'discounts#edit'
+     put '/discounts/:id', to: 'discounts#update'
+     patch '/discounts/:id', to: 'discounts#update'
+     post '/discounts', to: 'discounts#create'
   end
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    # resources :merchants, only: [:show, :update]
+    get '/merchants/:id', to: 'merchants#show'
+    put '/merchants/:id', to: 'merchants#update'
+    patch '/merchants/:id', to: 'merchants#update'
+
+    # resources :users, only: [:index, :show]
+    get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
+
     patch '/orders/:id/ship', to: 'orders#ship'
   end
 end
